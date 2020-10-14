d4b26e743ea12ea84c9cb4bb326e7921888bf135
kishorThatavarthi <t.kishor8121@gmail.com>
2020-10-14 16:18:32 +0530

    2 files modified, 1 added, 2 deleted

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from './hero';
+import {Hero} from '../hero';
 import { HEROES } from './mock-heros';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 @Component({
   selector: 'app-heros',
